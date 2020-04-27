ebc0feebd74b7184bc47044a112b0124fbdbd240
RenukaJayachandran <renu.tharini.jay@gmail.com>
2020-04-27 14:53:56 +0530

    added monoid example

--- sample.js
+++
@@ -1,5 +0,0 @@
-var x = new Array(10);
-for (var index = 0; index < x.length; index++) {
-    x[index] = Math.round(Math.random() * (x.length - 2)) + 1;
-}
-x.map(function (x) { return console.log(x); });
--- sample.ts
+++
@@ -1,7 +0,0 @@
-
-// let x = new Array(100);
-// for (let index = 0; index < x.length; index++) {
-//     x[index] =  Math.round(Math.random()*(x.length - 2))+1  
-// }
-// x.map(x => console.log(x))
-
